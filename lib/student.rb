76969e39250abd1412717c96d59c32e12dd8d1f6
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 00:39:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -21,4 +21,59 @@ class Student
     DB[:conn].execute(sql)
   end 
 
+  def self.drop_table
+    sql = <<-SQL 
+      DROP TABLE students 
+    SQL
+    
+    DB[:conn].execute(sql)
+  end 
+
+  def save 
+    if self.id
+      sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.album, self.id)
+    else 
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end 
+  end 
+  
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end
+  
+  def self.new_from_db(row)
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student 
+  end 
+  
+  def self.find_by_name(row)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      Limit 1 
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end 
+  
+  def update 
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end 
+  
 end
